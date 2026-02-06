c29349e70caa55f0fa2180fbf8640bc566c9e8f5
Aravind Srinivasan <aravindharini@gmail.com>
2026-02-06 01:41:43 +0000

    File changes

--- src/components/provider/ProviderBookingsTable.jsx
+++ src/components/provider/ProviderBookingsTable.jsx
@@ -20,7 +20,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import ServiceProviderChat from "@/components/provider/ServiceProviderChat";
+import ServiceProviderChat from "@/components/chat/ServiceProviderChat";
 import { format } from "date-fns";
 import { toast } from "react-hot-toast";
 
